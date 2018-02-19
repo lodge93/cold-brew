a88cf8df34a710b1928c94b0e4b6e9beb2bfeed7
Mark Spicer <spicer93@gmail.com>
2018-02-19 18:33:49 +0000

    Added base HTTP interface.
    This commit adds a base HTTP interface for the cold brew dripper and has heavy
    refactors to the dripper package as I was still figuring out how the package
    should function.

--- dripper/dripper.go
+++ dripper/dripper.go
@@ -32,6 +32,18 @@ const (
 	// Number of milliseconds per second which is used to convert between
 	// milliseconds and seconds.
 	millisecondsPerSec = 1000
+
+	// Used for internal state tracking. The run state represents the pump fully
+	// on, which is useful for blooming, priming, and draining the pump.
+	RUN = "run"
+
+	// Used for internal state tracking. The drip state represents the pump
+	// in the dripping state.
+	DRIP = "drip"
+
+	// Used for internal state tracking. The off state represents the pump fully
+	// stopped.
+	OFF = "off"
 )
 
 // Dripper is the base object used to implement methods to control the cold brew
@@ -56,18 +68,21 @@ type Dripper struct {
 	// A mutex used to lock the dipper object when updating the drips per minute
 	// outside the drip goroutine.
 	mutex sync.Mutex
+
+	// State is used internally to track the state of the dripper hardware.
+	state string
 }
 
 // New creates a new dripper instance and initializes the motor HAT driver.
 func New() (*Dripper, error) {
-	var d Dripper
-
-	// This is the motor number on the Adafruit Motor HAT indexed on zero. In
-	// this case, 2 is M3.
-	d.motorNum = 2
-
 	r := raspi.NewAdaptor()
-	d.pump = i2c.NewAdafruitMotorHatDriver(r)
+
+	d := Dripper{
+		motorNum:    2,
+		pump:        i2c.NewAdafruitMotorHatDriver(r),
+		state:       OFF,
+		stopDripper: make(chan bool),
+	}
 
 	// TODO: The start method attempts to initialize both the servo and motor
 	// drivers on the HAT. This is problematic because this package only uses
@@ -79,42 +94,23 @@ func New() (*Dripper, error) {
 		return &d, err
 	}
 
-	d.stopDripper = make(chan bool)
-
 	return &d, nil
 }
 
-// TODO: this will likely need refactored as there is no current way to stop
-// a bloom. In addition, the bloom duration would logically mean the amount of
-// time to wait after the water has been added where it is the amount of time
-// the motor is on in the current state.
-func (d *Dripper) Bloom(dur time.Duration) error {
-	err := d.setSpeed(maxSpeed)
-	if err != nil {
-		return err
-	}
-
-	err = d.on()
-	if err != nil {
-		return err
-	}
-
-	time.Sleep(dur)
-
-	err = d.off()
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
+// Drip starts the dripper at the desired drip rate.
 func (d *Dripper) Drip(dripsPerMin float64) error {
 	err := d.setSpeed(dripSpeed)
 	if err != nil {
 		return err
 	}
 
+	// This is a sanity check to ensure the dripper is not already in the drip
+	// state.
+	if d.state == DRIP {
+		return nil
+	}
+
+	d.state = DRIP
 	d.dripsPerMin = dripsPerMin
 
 	d.dripperWG.Add(1)
@@ -123,25 +119,80 @@ func (d *Dripper) Drip(dripsPerMin float64) error {
 	return nil
 }
 
-func (d *Dripper) Stop() error {
-	d.stopDripper <- true
-	d.dripperWG.Wait()
+// Run turns on the dripper at the maximum pump speed. This is useful for
+// blooming the batch of coffee, priming the pump with water, and draining the
+// pump when finished brewing.
+func (d *Dripper) Run() error {
+	// This is a sanity check to ensure the drip goroutine is stopped before
+	// trying to control the pump. This prevents the weird state where the pump
+	// is on the maximum speed, but is still pulsing from the drip goroutine.
+	if d.state == DRIP {
+		d.Off()
+	}
 
-	return d.off()
+	err := d.setSpeed(maxSpeed)
+	if err != nil {
+		return err
+	}
+
+	d.state = RUN
+
+	err = d.on()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
+// Off ensures the dripper is completely stopped.
+func (d *Dripper) Off() error {
+	if d.state == DRIP {
+		d.stopDripper <- true
+		d.dripperWG.Wait()
+	}
+	d.state = OFF
+
+	return d.stop()
+}
+
+// SetDripsPerMinute will update the dripper with the desired drip rate.
 func (d *Dripper) SetDripsPerMinute(dripsPerMin float64) {
 	d.mutex.Lock()
 	d.dripsPerMin = dripsPerMin
 	d.mutex.Unlock()
 }
 
-// TODO: this method relies heavily on time.Sleep which is problematic as it
-// does not lead to a highly accurate drip rate. For example, 60 drips at 60
-// drips per minute will execute in ~60.5 seconds. While good enough for the
-// intial iteration, this should be refactored to reflect a more accurate drip
-// rate as this will run for several hours and could drift quite a bit.
+// GetState returns the internal state of the dripper. This is useful for
+// clients to determine the state of the dripper when reconnecting.
+func (d *Dripper) GetState() string {
+	// TODO: The mutexes in this package need updated so there are two seperate
+	// mutexes. Also, this will need a mutex wrapper once there are two seperate
+	// mutexes.
+	return d.state
+}
+
+// GetDripsPerMinute will return the current drip rate from the dripper.
+func (d *Dripper) GetDripsPerMinute() float64 {
+	d.mutex.Lock()
+	dpm := d.dripsPerMin
+	d.mutex.Unlock()
+
+	return dpm
+}
+
+// runDrip runs a goroutine to pulse the dripper at the desired drip rate. This
+// is mainly to account for the fact that the cheap peristaltic pump used in
+// this design as the motor cannot rotate much lower then the drip speed
+// constant. In order to get just a small amount of water out of the pump, it is
+// simply turned on at the lowest speed for some amount of time before being
+// stopped to simulate a single drip.
 func (d *Dripper) runDrip() {
+	// TODO: this method relies heavily on time.Sleep which is problematic as it
+	// does not lead to a highly accurate drip rate. For example, 60 drips at 60
+	// drips per minute will execute in ~60.5 seconds. While good enough for the
+	// intial iteration, this should be refactored to reflect a more accurate
+	// drip rate as this will run for several hours and could drift quite a bit.
 	defer d.dripperWG.Done()
 
 	for {
@@ -156,7 +207,7 @@ func (d *Dripper) runDrip() {
 
 			time.Sleep(dripDuration * time.Millisecond)
 
-			err = d.off()
+			err = d.stop()
 			if err != nil {
 				log.Println(err)
 			}
@@ -167,25 +218,31 @@ func (d *Dripper) runDrip() {
 			return
 		default:
 			d.mutex.Lock()
-			stopDuration := calcStopDuration(d.dripsPerMin)
+			dpm := d.dripsPerMin
 			d.mutex.Unlock()
+			stopDuration := calcStopDuration(dpm)
 			time.Sleep(time.Duration((stopDuration * 1000)) * time.Millisecond)
 		}
 	}
 }
 
+// on is a low level mehtod to start the rotation of the motor.
 func (d *Dripper) on() error {
 	return d.pump.RunDCMotor(d.motorNum, i2c.AdafruitForward)
 }
 
+// setSpeed is a low level method to set the motor speed.
 func (d *Dripper) setSpeed(speed int32) error {
 	return d.pump.SetDCMotorSpeed(d.motorNum, speed)
 }
 
-func (d *Dripper) off() error {
+// stop is a low level method to stop the rotation of the motor.
+func (d *Dripper) stop() error {
 	return d.pump.RunDCMotor(d.motorNum, i2c.AdafruitRelease)
 }
 
+// calcStopDuration calculates the amount of time between drips is necessary
+// to achieve the desired drip rate.
 func calcStopDuration(dripsPerMin float64) float64 {
 	if dripsPerMin > 240 {
 		dripsPerMin = 240
