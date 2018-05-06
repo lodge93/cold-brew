74d0e01b5a72d5d9f829281a405c6e1b09b447e5
Mark Spicer <spicer93@gmail.com>
2018-05-06 14:35:38 -0400

    Added base application configuration.
    This commit adds a base configuration file for the cold brew application.

---
+++ server/config_test.go
@@ -0,0 +1,43 @@
+// Copyright © 2018 Mark Spicer
+// Made available under the MIT license.
+
+package server
+
+import (
+	"testing"
+)
+
+func TestNewConfigReadsFromTestData(t *testing.T) {
+	config, err := NewConfig()
+	if err != nil {
+		t.Error("could not load configuration file:", err)
+	}
+
+	if config.DBFile != "/foo/bar" {
+		t.Error("incorrect database file path was loaded from config file")
+	}
+
+	if config.Environment != "testing" {
+		t.Error("incorrect environment loaded from config file")
+	}
+}
+
+func TestIsValidEnvironmentWhenEnvironmentIsValid(t *testing.T) {
+	if !isValidEnvironment(EnvDevelopment) {
+		t.Error("valid environment did not return as valid")
+	}
+
+	if !isValidEnvironment(EnvProduction) {
+		t.Error("valid environment did not return as valid")
+	}
+
+	if !isValidEnvironment(EnvTesting) {
+		t.Error("valid environment did not return as valid")
+	}
+}
+
+func TestIsValidEnvironmentWhenEnvironmenIsNotValid(t *testing.T) {
+	if isValidEnvironment("foo") {
+		t.Error("invalid environment returned as valid")
+	}
+}
